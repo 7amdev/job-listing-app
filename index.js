e7cb392d0efa57261396357fbb770473a9d0df96
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2023-10-02 11:12:20 -0100

    feat: render job posts; fetch results by query;

--- index.js
+++ index.js
@@ -21,11 +21,17 @@ search_el.addEventListener('submit', function (event) {
     return;
   } 
 
+  if (search_input_value.length === 0) {
+    console.log('Invalid search text...');
+    // Show Message error dialog
+    return;
+  }
+
   // show loading spinner...
   spinner_job_list_el.classList.add('spinner--visible');
 
   // fetch call
-  fetch('http://localhost:3000/jobs')
+  fetch(`http://localhost:3000/jobs?q=${search_input_value}`)
   .then(function (response) {
     if (!response.ok) {
       throw new Error('Response error (e.g Netwok or server problem occured...)');
@@ -34,28 +40,53 @@ search_el.addEventListener('submit', function (event) {
     return response.json();
   })
   .then(function (data) {
+
     // hide spinner
     spinner_job_list_el.classList.remove('spinner--visible');
 
     // render # of results 
-    job_results_count_el.textContent = data.jobItems.length;
+    job_results_count_el.textContent = data.length;
 
     // clear job list
     job_post_list_el.innerHTML = '';
 
-
-    // render job items. show 
-    // only 7 items
-
-    console.log(data);
+    // render job items
+    data.forEach(function (job_item) {
+      const job_post_markup = 
+        `<li class="job-post">
+            <p class="badge">${job_item.badgeLetters}</p>
+            <div class="job-post__content">
+              <h4 class="job-post__title">${job_item.title}</h4>
+              <p class="job-post__company">${job_item.company}</p>
+              <ul class="job-post__info">
+                <li class="job-post__type">
+                  <i class="fa-solid fa-clock job-post__icon"></i>
+                  ${job_item.duration}
+                </li>
+                <li class="job-post__salary">
+                  <i class="fa-solid fa-money-bill job-post__icon"></i>
+                  $${job_item.salary}
+                </li>
+                <li class="job-post__location">
+                  <i class="fa-solid fa-location-dot job-post__icon"></i>
+                  ${job_item.location}
+                </li>
+              </ul>
+            </div>
+            <div class="job-post__meta">
+              <button class="bookmark">
+                <i class="fa-solid fa-bookmark bookmark__icon"></i>
+              </button>
+              <p class="job-post__date">${job_item.daysAgo}d</p>
+            </div>
+          </li>`;
+  
+      job_post_list_el.insertAdjacentHTML('beforeend', job_post_markup);
+    });
   })
   .catch(function (error) {
     console.warn(error.message);
     // show dialog messsage
   });
-
-  
-
-  
 });
 
