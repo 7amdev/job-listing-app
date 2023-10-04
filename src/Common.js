f31292d2ab1c8779b2d52dba56ae51d17393d271
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2023-10-04 10:12:31 -0100

    refactor: modularize the app structure

---
+++ src/Common.js
@@ -0,0 +1,8 @@
+export const search_el = document.querySelector('.search');
+export const search_input_el = document.querySelector('.search__input');
+export const job_results_count_el = document.querySelector('.filter__job-count');
+export const job_post_list_el = document.querySelector('.job-post-list');
+export const spinner_job_list_el = document.querySelector('.spinner--job-list');
+export const spinner_job_details_el = document.querySelector('.spinner--job-details');
+export const job_details_el = document.querySelector('.job-details');
+export const job_details_content_el = document.querySelector('.job-details__content');
