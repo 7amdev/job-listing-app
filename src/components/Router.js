e3b70d4fdfde58288b92fcaa9f126c3e07b607ee
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2023-10-11 12:01:01 -0100

    fix: add hashchange event listener

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,17 +5,23 @@ const router = function () {
 
 // REGISTERING EVENTS
 window.addEventListener('DOMContentLoaded', router);
-window.addEventListener('popstate', router);
-document && document.addEventListener('click', function (event) {
-  const { target } = event;
-  
-  if (!target.closest('a')) return;
+window.addEventListener('hashchange', router);
 
-  event.preventDefault();
+// SINCE our app uses HASH, this code is not necessary
 
-  console.log('Anchor tag has been clicked...');
+// window.addEventListener('popstate', router);
 
-  // Register href attribute to Browser History Api
+// document && document.addEventListener('click', function (event) {
+//   const { target } = event;
+//   const anchor_el = target.closest('a');
 
-  // Call Router to find a math and render the view
-});
+//   if (!anchor_el) return;
+
+//   event.preventDefault();
+
+//   // Register href attribute to Browser History Api
+//   history.pushState(null, null, anchor_el.href);
+
+//   // Call Router to find a math and render the view
+//   router();
+// });
