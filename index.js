804770d2f53c31db8accbd5bddb3837e3194c20c
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2023-10-23 10:02:33 -0100

    refactor: move index files into source folder

--- index.js
+++
@@ -1,7 +0,0 @@
-import './src/common.js';
-import './src/components/Search.js';
-import './src/components/JobList.js';
-import './src/components/Sort.js';
-import './src/components/Pagination.js';
-import './src/components/Router.js';
-import './src/components/BookmarkDropdown.js';
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './common.js';
+import './components/Search.js';
+import './components/JobList.js';
+import './components/Sort.js';
+import './components/Pagination.js';
+import './components/Router.js';
+import './components/BookmarkDropdown.js';
