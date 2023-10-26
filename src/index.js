d5c3aad17ded451a04c5ffb1b63ee126d354b249
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2023-10-26 10:19:49 -0100

    move index.html to production dist folder;include css to the build processs;

--- src/index.js
+++ src/index.js
@@ -6,6 +6,4 @@ import './components/Pagination.js';
 import './components/Router.js';
 import './components/BookmarkDropdown.js';
 
-// FOR PRODUCTION BUILD ONLY
-// [link] https://web.dev/articles/css-module-scripts
-import './index.css' assert { type: 'css' };;
+import './index.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// @todo
+// [link] https://dev.to/alecgodwin/how-to-setup-webpack-and-babel-for-es6-dpk
 
 module.exports = {
   mode: 'production',
@@ -9,6 +12,10 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'main.css'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
     })
   ],
   module: {
